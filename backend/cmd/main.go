ed06ac32e905971e3397f7d11f42250857918261
iZarrios <zarriose@gmail.com>
2023-07-03 18:30:07 +0300

    add: migrate flag

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/iZarrios/link-shortener/backend/pkg/db"
+	"github.com/iZarrios/link-shortener/backend/pkg/routes"
 	"github.com/iZarrios/link-shortener/backend/pkg/types"
 	"github.com/joho/godotenv"
 )
@@ -17,12 +21,63 @@ func main() {
 		panic("Error creating store")
 	}
 
-	db.AutoMigrate(&types.Link{})
+	toMigrate := flag.Bool("migrate", false, "set to true to migrate")
+	flag.Parse()
 
-	link := &types.Link{
-		Url:      "https://www.google.com",
-		Redirect: "no",
-		Count:    0,
+	if *toMigrate {
+		fmt.Println("Migrating...")
+		// add new types here
+		/////////////////////////
+		types := []interface{}{
+			&types.Link{},
+		}
+		/////////////////////////
+		db.AutoMigrate(types...)
+		fmt.Println("Migration complete")
 	}
-	db.Save(&link)
+
+	app := NewServer(db)
+    // setting up middlewares
+
+
+    // setting up routes
+	// swagger
+	// public
+	// private
+	// not found
+	app.Use(routes.NotFoundRoute)
+
+	app.Listen(fmt.Sprintf("%s:%s", app.Host, app.Port))
+	// rows, err := db.DB.Raw("SELECT * FROM links").Rows()
+	// if err != nil {
+	//     panic(err)
+	// }
+	// defer rows.Close()
+	// for rows.Next() {
+	//     var id uint64
+	//     var url string
+	//     var redirect string
+	//     var count uint64
+	//     var created_at string
+	//     var updated_at string
+	//     err = rows.Scan(&id, &url, &redirect, &count, &created_at, &updated_at)
+	//     if err != nil {
+	//         panic(err)
+	//     }
+	//     println(id, url, redirect, count, created_at, updated_at)
+	// }
+
+	// get all records in links table
+	// var links []types.Link
+	// db.Find(&links)
+	// for _, link := range links {
+	//
+	//     // created_at := time.Time(link.CreatedAt).Format("2006-01-02 15:04:05")
+	//     // updated_at := time.Time(link.UpdatedAt).Format("2006-01-02 15:04:05")
+	//     link.CreatedAt = time.Time(link.CreatedAt)
+	//     link.UpdatedAt = time.Time(link.UpdatedAt).Add(time.Hour * 8)
+	//     fmt.Print(link.CreatedAt, link.UpdatedAt)
+	//
+	//     // println(link.ID, link.Url, link.Redirect, link.Count, created_at, updated_at)
+	// }
 }
