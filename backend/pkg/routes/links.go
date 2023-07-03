456c1885717d2f3b4d0a0fa17a7e401bd430d8a6
iZarrios <zarriose@gmail.com>
2023-07-03 23:04:42 +0300

    add: RD in CRUD

--- backend/pkg/routes/links.go
+++ backend/pkg/routes/links.go
@@ -1,8 +1,11 @@
 package routes
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/iZarrios/link-shortener/backend/pkg/db"
+	"github.com/iZarrios/link-shortener/backend/pkg/types"
 )
 
 func SetupLinksRoutes(app fiber.Router, db *db.SqlStore) {
@@ -16,14 +19,17 @@ func SetupLinksRoutes(app fiber.Router, db *db.SqlStore) {
 
 func getLinks(db *db.SqlStore) func(c *fiber.Ctx) error {
 
+	var links []types.Link
+	db.Find(&links)
+
 	response := &ApiResponse{
-		Code:  fiber.StatusOK,
-		Msg:   "Links retrieved successfully",
-		Error: true,
-		Data:  nil,
+		Code: fiber.StatusOK,
+		Msg:  "Links retrieved successfully",
+		Data: links,
 	}
 	return func(c *fiber.Ctx) error { // return of the function is a fiber handler
-		return c.JSON(response)// return of the handler is an error
+		c.Status(response.Code)
+		return c.JSON(response) // return of the handler is an error
 
 	}
 }
@@ -35,19 +41,52 @@ func createLink(db *db.SqlStore) func(*fiber.Ctx) error {
 }
 
 func getLink(db *db.SqlStore) func(*fiber.Ctx) error {
+	response := &ApiResponse{
+		Code: fiber.StatusOK,
+		Msg:  "Link retrieved successfully",
+		Data: nil,
+	}
 	return func(c *fiber.Ctx) error {
-		return c.SendString("getLink")
+		id, err_conv := strconv.Atoi(c.Params("id"))
+
+		var link *types.Link
+		err := db.First(&link, id).Error
+		if err != nil || err_conv != nil {
+			response.Msg = "Link not found"
+		} else {
+			c.Status(response.Code)
+			response.Data = link
+		}
+		return c.JSON(response)
 	}
 }
 
 func updateLink(db *db.SqlStore) func(*fiber.Ctx) error {
+    response := &ApiResponse{
+        Code: fiber.StatusOK,
+        Msg:  "Link updated successfully",
+        Data: nil,
+    }
 	return func(c *fiber.Ctx) error {
-		return c.SendString("updateLink")
+        return c.JSON(response)
 	}
 }
 
 func deleteLink(db *db.SqlStore) func(*fiber.Ctx) error {
+    response := &ApiResponse{
+        Code: fiber.StatusOK,
+        Msg:  "Link deleted successfully",
+        Data: nil,
+    }
 	return func(c *fiber.Ctx) error {
-		return c.SendString("deleteLink")
+        id, err_conv := strconv.Atoi(c.Params("id"))
+        err :=db.Delete(&types.Link{}, id).Error
+        if err != nil || err_conv != nil {
+            response.Msg = "Link not found"
+        } else {
+            c.Status(response.Code)
+            response.Data = id 
+        }
+        return c.JSON(response)
 	}
 }
