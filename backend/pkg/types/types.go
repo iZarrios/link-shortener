129f49dcf86a38ccf2d6881713c510e1930d3ac4
iZarrios <zarriose@gmail.com>
2023-07-06 18:52:01 +0300

    bit: json tag update

--- backend/pkg/types/types.go
+++ backend/pkg/types/types.go
@@ -7,16 +7,16 @@ import (
 )
 
 type Link struct {
-    ID uint64 `json:"id" gorm:"primary_key"`
-    Url string `json:"url" gorm:"not null unique"`
-    Redirect string `json:"short_url" gorm:"not null unique"`
-    Count uint64 `json:"count" gorm:"not null"`
-    CreatedAt time.Time `json:"created_at" gorm:"not null default CURRENT_TIMESTAMP"`
-    UpdatedAt time.Time `json:"updated_at" gorm:"not null default CURRENT_TIMESTAMP "`
+	ID             uint64    `json:"id" gorm:"primary_key"`
+	Url            string    `json:"url" gorm:"not null unique"`
+	Redirect       string    `json:"redirect" gorm:"not null unique"`
+	NumberOfVisits uint64    `json:"number_of_visits" gorm:"not null"`
+	CreatedAt      time.Time `json:"created_at" gorm:"not null default CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time `json:"updated_at" gorm:"not null default CURRENT_TIMESTAMP "`
 }
 
 type APISever struct {
-    * fiber.App
-    Port string
-    Host string
+	*fiber.App
+	Port string
+	Host string
 }
